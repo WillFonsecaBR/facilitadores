21a3bc6c03271ec17feb676cd4d5b649e7af85b0
Willian Fonseca <willian.alves2109@gmail.com>
2026-06-24 17:59:28 -0300

    ADICIONANDO ADICIONADOR DE PASTAS & ORGANIZADOR DE VIDEOS PARA EDIÇÃO

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from separador_de_videos import separar_videos
 from selecionador import run_selecionador
 from criador_pastas import run_criador_pastas
 from organizador_videos_edicao import run_organizador_videos
+from movendo_videos_marca_dagua import run_movendo_videos_marca_dagua
 
 def carregar_config():
     with open('config.json', 'r', encoding='utf-8') as f:
@@ -18,20 +19,25 @@ def menu():
         print('3. Selecionador de Imagens')
         print('4. Criador de Subpastas (Finalizados/MD)')
         print('5. Organizador de Vídeos para Edição')
-        print('6. Sair') 
+        print('6. Mover Vídeos com Marca D\'Água')
+        print('7. Sair')
+
         escolha = input('Escolha uma opção: ')
+
         if escolha == '1':
             try:
                 run_organizador()
                 print('Organização concluída.')
             except Exception as e:
                 print(f'Erro: {e}')
+
         elif escolha == '2':
             try:
                 separar_videos()
                 print('Separação concluída.')
             except Exception as e:
                 print(f'Erro: {e}')
+
         elif escolha == '3':
             try:
                 print('Iniciando Selecionador de Imagens...')
@@ -39,6 +45,7 @@ def menu():
                 print('\nProcesso finalizado.')
             except Exception as e:
                 print(f'Erro: {e}')
+
         elif escolha == '4':
             try:
                 print('Iniciando Criador de Pastas...')
@@ -46,6 +53,7 @@ def menu():
                 print('\nProcesso finalizado.')
             except Exception as e:
                 print(f'Erro: {e}')
+
         elif escolha == '5':
             try:
                 print('Iniciando Organizador de Vídeos para Edição...')
@@ -53,7 +61,16 @@ def menu():
                 print('\nProcesso finalizado.')
             except Exception as e:
                 print(f'Erro: {e}')
+
         elif escolha == '6':
+            try:
+                print('Iniciando Movimentação de Vídeos com Marca D\'Água...')
+                run_movendo_videos_marca_dagua()
+                print('\nProcesso finalizado.')
+            except Exception as e:
+                print(f'Erro: {e}')
+
+        elif escolha == '7':
             print('Saindo...')
             break
         else:
